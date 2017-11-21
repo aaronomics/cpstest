2c64fca3df2e3f60199b083feb2166279dac357e
Aaron <aaronwweiss00@gmail.com>
2017-11-21 17:39:34 -0500

    website css and js files

---
+++ custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+    
+        var userFeed = new Instafeed({
+            //get: 'user',
+            get: 'tagged',
+            tagName: 'hockey',
+            clientId: '1533937457',
+            limit: 4,
+            resolution: 'thumbnail',
+            //accessToken: '1533937457.1677ed0.948fc4bd116f4423aca10cbc61a468c2',
+            sortBy: 'most-recent',
+            template: '<div class="col-lg-3 gallery instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+        });
+    
+    
+        userFeed.run();
+    })
